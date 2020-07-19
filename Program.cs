037a9f805ae1bb5bb189536bf910bf71169a39ef
Tai Thien Bo <taithienbo@gmail.com>
2020-07-19 13:19:37 -0700

    Set up CI with Azure Pipelines
    [skip ci]
    
    Fix project structure

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentTime = DateTime.Now;
+            var holder = "";
+            if (currentTime.Hour < 12)
+            {
+                holder = "morning";
+            }
+            else if (currentTime.Hour > 12 && currentTime.Hour < 17)
+            {
+                holder = "afternoon";
+            }
+            else
+            {
+                holder = "Evening";
+            }
+            using (var fs = new StreamWriter("output.txt", true))
+            {
+                fs.WriteLine($"Good {holder}. The time is {currentTime}");
+            }          
+        }
+    }
+}
